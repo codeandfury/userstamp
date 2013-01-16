d5d15b505713be47aed490fd2f0510ceeff8fc7e
Winton DeShong <contact@wintondeshong.com>
2013-01-16 14:28:26 -0500

    added tmp directory to gitignore; refactored specs to use let and also use local variables where appropriate

--- spec/lib/userstamp_spec.rb
+++ spec/lib/userstamp_spec.rb
@@ -16,14 +16,14 @@ class Ping < ActiveRecord::Base
 end
 
 describe "Userstamp" do
-  before(:each) do
+  before(:all) do
     @zeus = User.create(id: 1, name: "Zeus")
     @hera = User.create(id: 2, name: "Hera")
   end
   
   describe "stampable" do
-    context "default" do
-      before(:each) do
+    context "default configuration" do
+      before(:all) do
         class Comment < ActiveRecord::Base
           stampable :stamper_class_name => :user
         end
@@ -32,16 +32,19 @@ describe "Userstamp" do
       end
       
       it "marks the creator_id and the updated_by" do
-        @comment = Comment.create(text: "basic comment")
-        expect(@comment.creator_id).to eq @zeus.id
-        expect(@comment.creator).to eq @zeus
-        expect(@comment.updated_by).to eq @zeus.id
-        expect(@comment.updater).to eq @zeus
+        comment = Comment.create(text: "basic comment")
+        expect(comment.creator_id).to eq @zeus.id
+        expect(comment.creator).to eq @zeus
+        expect(comment.updated_by).to eq @zeus.id
+        expect(comment.updater).to eq @zeus
       end
     end
     
-    context "compatibility mode is true" do
-      before(:each) do
+    context "compatibility mode is enabled" do
+
+      let(:person) { Person.create(name: "Josh") }
+
+      before(:all) do
         # Class must be re-defined after compatibility_mode is changed
         Ddb::Userstamp.compatibility_mode = true
         class Person < ActiveRecord::Base
@@ -52,57 +55,54 @@ describe "Userstamp" do
       end
       
       it "marks the created_by field" do 
-        @person = Person.create(name: "Josh")
-        expect(@person.created_by).to eq @zeus.id
-        expect(@person.creator).to eq @zeus
+        expect(person.created_by).to eq @zeus.id
+        expect(person.creator).to eq @zeus
       end
       
       it "marks the updated_by field" do
-        @person = Person.create(name: "Josh")
-        @person.name = "Dave"
-        @person.save
-        expect(@person.name).to eq "Dave"
-        expect(@person.updated_by).to eq @zeus.id
-        expect(@person.updater).to eq @zeus
+        person.name = "Dave"
+        person.save
+        expect(person.name).to eq "Dave"
+        expect(person.updated_by).to eq @zeus.id
+        expect(person.updater).to eq @zeus
       end
       
       it "marks the deleted_by field" do
-        @person = Person.create(name: "Josh")
-        expect(@person.name).to eq "Josh"
-        @person.destroy
-        expect(@person.deleted_by).to eq @zeus.id
-        expect(@person.deleter).to eq @zeus
+        expect(person.name).to eq "Josh"
+        person.destroy
+        expect(person.deleted_by).to eq @zeus.id
+        expect(person.deleter).to eq @zeus
       end
     end
     
-    context "compatibility mode is false" do
-      before(:each) do
+    context "compatibility mode is disabled" do
+
+      let(:post) { Post.create(title: "Sample One") }
+
+      before(:all) do
         # This is the default value for the compatibility mode
         # Ddb::Userstamp.compatibility_mode = false
         User.stamper = @zeus
       end
-      
+
       it "marks the created_by field" do 
-        @post = Post.create(title: "Sample One")
-        expect(@post.creator_id).to eq @zeus.id
-        expect(@post.creator).to eq @zeus
+        expect(post.creator_id).to eq @zeus.id
+        expect(post.creator).to eq @zeus
       end
       
       it "marks the updated_by field" do
-        @post = Post.create(title: "Sample One")
-        @post.title = "Sample Two"
-        @post.save
-        expect(@post.title).to eq "Sample Two"
-        expect(@post.updater_id).to eq @zeus.id
-        expect(@post.updater).to eq @zeus
+        post.title = "Sample Two"
+        post.save
+        expect(post.title).to eq "Sample Two"
+        expect(post.updater_id).to eq @zeus.id
+        expect(post.updater).to eq @zeus
       end
       
       it "marks the deleted_by field" do
-        @post = Post.create(title: "Sample One")
-        expect(@post.title).to eq "Sample One"
-        @post.destroy
-        expect(@post.deleter_id).to eq @zeus.id
-        expect(@post.deleter).to eq @zeus
+        expect(post.title).to eq "Sample One"
+        post.destroy
+        expect(post.deleter_id).to eq @zeus.id
+        expect(post.deleter).to eq @zeus
       end
     end
     
